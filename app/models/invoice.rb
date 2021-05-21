cdc93b13d87de767b05f10c7ef8ce27f9ea4e997
George Ho <george.h.w.ho@gmail.com>
2021-05-21 14:18:18 -0400

    Add model validations and relationship tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
+  validates :status, presence: true
+
+  has_many :invoice_items, dependent: :destroy
+
   belongs_to :merchant
   belongs_to :customer
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+
+  has_many :invoice_items, dependent: :destroy
+
   belongs_to :merchant
 end
