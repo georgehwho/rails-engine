20898e1fc5ac003fb15ad49f0a3081cbfce88feb
George Ho <george.h.w.ho@gmail.com>
2021-05-25 22:56:58 -0400

    Add 4 of the AR queries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/revenue/merchants', to: 'revenue#merchants_revenue'
+      get '/revenue/merchants/:id', to: 'revenue#merchant_revenue'
+
+      get '/revenue/items', to: 'revenue#items_revenue'
+      get '/merchants/most_items', to: 'merchants#items_sold'
+
       get '/merchants/find', to: 'merchants#find'
+
       get '/items/find_all', to: 'items#find_all'
 
       resources :merchants, only: [:index, :show] do
